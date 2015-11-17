e18dc3772bc6f455341896ffffcf69d6b316ac79
Anis Abid <anis.abid-ext@amundi.com>
2015-11-17 16:19:49 +0100

    inti project

---
+++ src/app/app.js
@@ -0,0 +1,54 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeitas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'Windstorm'
+        };
+        this.heroes = HEROES;
+    }
+    AppComponent.prototype.onSelectHero = function (hero) {
+        this.selectHero = hero;
+    };
+    AppComponent.prototype.getSelectedClass = function (hero) {
+        return { 'selected': hero === this.selectHero };
+    };
+    AppComponent = __decorate([
+        angular2_1.Component({
+            styles: ["\n\t\t.heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n\t\t.heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n\t\t.heroes li:hover {color: #369; background-color: #EEE; left: .2em;}\n\t\t.heroes .badge {\n\t\tfont-size: small;\n\t\tcolor: white;\n\t\tpadding: 0.1em 0.7em;\n\t\tbackground-color: #369;\n\t\tline-height: 1em;\n\t\tposition: relative;\n\t\tleft: -1px;\n\t\ttop: -1px;\n\t\t}\n\t\t.selected { background-color: #EEE; color: #369; }\n  \t"],
+            selector: 'my-app',
+            template: "\n\t\t\t<h1>{{title}}</h1>\n\t\t\t<div *ng-if=\"selectHero\">\n\t\t\t\t<h2>{{selectHero.name}} details!</h2>\n\t\t\t\t<div>\n\t\t\t\t\t<label>id: </label>{{selectHero.id}}\n\t\t\t\t</div>\n\t\t\t\t<div>\n\t\t\t\t\t<label>name: </label>\n\t\t\t\t\t<div>\n\t\t\t\t\t\t<input [(ng-model)]=\"selectHero.name\" placeholder=\"name\">\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<h2>My Heroes</h2>\n\t\t\t<ul class=\"heroes\">\n\t\t\t\t<li \n\t\t\t\t\t*ng-for=\"#hero of heroes; #i = index\" \n\t\t\t\t\t(click)=\"onSelectHero(hero)\"\n\t\t\t\t\t[ng-class]=\"getSelectedClass(hero)\">\n\t\t\t\t\t{{i + 1}} - <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t\t\t<!-- each hero goes here -->\n\t\t\t\t</li>\n\t\t\t</ul>\n\t\t\t",
+            directives: [angular2_1.FORM_DIRECTIVES, angular2_1.NgFor, angular2_1.NgIf, angular2_1.NgClass]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+})();
+angular2_1.bootstrap(AppComponent);
+//# sourceMappingURL=app.js.map
---
+++ src/app/app.ts
@@ -0,0 +1,85 @@
+import {bootstrap, Component, FORM_DIRECTIVES, NgFor, NgIf, NgClass} from 'angular2/angular2';
+
+class Hero {
+  id: number;
+  name: string;
+}
+var HEROES: Hero[] = [
+	{"id":11, "name": "Mr. Nice"},
+	{"id":12, "name": "Narco"},
+	{"id":13, "name": "Bombasto"},
+	{"id":14, "name": "Celeitas"},
+	{"id":15, "name": "Magneta"},
+	{"id":16, "name": "RubberMan"},
+	{"id":17, "name": "Dynama"}
+];
+@Component({
+	styles:[`
+		.heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+		.heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+		.heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+		.heroes .badge {
+		font-size: small;
+		color: white;
+		padding: 0.1em 0.7em;
+		background-color: #369;
+		line-height: 1em;
+		position: relative;
+		left: -1px;
+		top: -1px;
+		}
+		.selected { background-color: #EEE; color: #369; }
+  	`],
+    selector: 'my-app',
+    template: `
+			<h1>{{title}}</h1>
+			<div *ng-if="selectHero">
+				<h2>{{selectHero.name}} details!</h2>
+				<div>
+					<label>id: </label>{{selectHero.id}}
+				</div>
+				<div>
+					<label>name: </label>
+					<div>
+						<input [(ng-model)]="selectHero.name" placeholder="name">
+					</div>
+				</div>
+			</div>
+			<h2>My Heroes</h2>
+			<ul class="heroes">
+				<li 
+					*ng-for="#hero of heroes; #i = index" 
+					(click)="onSelectHero(hero)"
+					[ng-class]="getSelectedClass(hero)">
+					{{i + 1}} - <span class="badge">{{hero.id}}</span> {{hero.name}}
+					<!-- each hero goes here -->
+				</li>
+			</ul>
+			`,
+	directives: [FORM_DIRECTIVES, NgFor, NgIf, NgClass]
+})
+
+
+
+// test commont
+class AppComponent {
+  public title = 'Tour of Heroes';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  public heroes = HEROES;
+
+  public selectHero: Hero;
+
+  onSelectHero(hero: Hero) {
+  	this.selectHero = hero;
+  }
+
+  getSelectedClass(hero: Hero){
+  return { 'selected': hero === this.selectHero }
+  }
+
+
+}
+bootstrap(AppComponent);
